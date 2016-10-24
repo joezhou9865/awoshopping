31192298f63d70645f7ded88a77d0bad8f2f955d
joe <986514728@qq.com>
2016-10-24 22:47:03 +0800

    第三次提交

--- awoshopping/www/src/JavaScript/indexdata.js
+++ awoshopping/www/src/JavaScript/indexdata.js
@@ -3,19 +3,24 @@ var loadIndexData = function(){
 		"src/datajson/index.json",
 		function(jsonData){
 			$(jsonData).each(function(i, n){
-				$("dl:eq(" + i + ") h3 a").html(n.title);
+				$(".dl-con:eq(" + i + ") h3 a").html(n.title);
 				});
 			$(jsonData).each(function(i,n){
-				$("dl:eq(" + i + ") .dt-span-a").css({background:"url("+n.imgsrc+")"});
+				$(".dl-con:eq(" + i + ") .dt-span-a").css({background:"url("+n.imgsrc+")"});
 			
 			});
 			$(jsonData).each(function(i,n){
-				$("dl:eq(" + i + ") .dt-span-p b").html("<i>￥</i>"+n.price+"<i>00</i>");
+				$(".dl-con:eq(" + i + ") .dt-span-p b").html("<i>￥</i>"+n.price+"<i>00</i>");
 			});
 			$(jsonData).each(function(i,n){
-				$("dl:eq(" + i + ") dd:eq(0) ").html("原价：<i>"+n.original+"</i>")
+				$(".floor-dd:eq(" + i + ") p:eq(0) ").html("原价：<i>"+n.original+"</i>")
 			});
-			
+			$(jsonData).each(function(i,n){
+				$(".floor-dd:eq(" + i + ") p:eq(1) ").html("折扣：<b>"+n.discount+"</b>折")
+			});
+			$(jsonData).each(function(i,n){
+				$(".floor-dd:eq(" + i + ") p:eq(2) ").html("<b>"+n.count+"</b>人已购买")
+			});
 			
 		}
 	)
